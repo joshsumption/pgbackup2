ea4070c08482da90a070ef729430557aa8c2eee8
Josh Sumption <joshsumptionmn@outlook.com>
2018-01-04 01:04:09 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    description='Database backups locally or to AWS S3.',
+    long_description=readme,
+    author='Josh Sumption',
+    author_email='joshsumptionmn@outlook.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[] 
+) 
+    
